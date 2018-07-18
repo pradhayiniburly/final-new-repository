0e6cb1dd70093b2e893824efa72dac04dcb4e6f3
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-6PF8H92.dir.svc.accenture.com>
2018-07-18 11:44:43 +0530

    node bidana

--- first_project/src/test/java/first_project/class1.java
+++ first_project/src/test/java/first_project/class1.java
@@ -3,15 +3,28 @@ package first_project;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class class1 {
+	@Parameters("browser")
 	@Test
-	public void open_driver ()
+	public void open_driver(String value)
 	{
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_b8c.07.19\\Downloads\\chromedriver_win32\\chromedriver.exe");
-	WebDriver driver=new ChromeDriver();	
+		WebDriver driver=null;
+		if(value.equals("chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\training_b8c.07.19\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if(value.equals("firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\training_b8c.07.19\\Downloads\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+			driver=new FirefoxDriver();
+		} 
+				
 	driver.get("https://github.com/login");
 	driver.findElement(By.id("login_field")).sendKeys("pradhayiniburly");
 	driver.findElement(By.id("password")).sendKeys("Git@123");
